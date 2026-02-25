d96363689268b72f488de041e72e474a747ce74a
andrewwang <andrewang562@gmail.com>
2026-02-24 18:09:58 -0800

    Imported motion library and added animations to intro button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { motion } from "motion/react"
 
 function App() {
 
   return (
     <>
-      <button className="introButton">Andrew Wang's Portfolio</button>
+      <motion.button 
+        initial={{ opacity: 0, scale: 0}}
+        animate={{ opacity: 1, scale: 1}}
+        whileHover={{ scale: 1.1}}
+        whileTap={{ scale: 0.9}}
+        className="introButton">Andrew Wang's Portfolio</motion.button>
     </>
   )
 }
